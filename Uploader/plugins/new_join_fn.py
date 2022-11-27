8cb9003180ebc7ab5765f63655fa635495a8beaf
Lx 0980 <116142186+0AIB@users.noreply.github.com>
2022-11-27 16:51:12 +0530

    Update new_join_fn.py

--- Uploader/plugins/new_join_fn.py
+++ Uploader/plugins/new_join_fn.py
@@ -1,7 +1,7 @@
 import logging
 
 import pyrogram
-from tobrot import *
+from Uploader import *
 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
@@ -15,7 +15,7 @@ async def new_join_f(client, message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton('Channel', url='https://t.me/MaxxBots')
+                        InlineKeyboardButton('Channel', url='https://t.me/DFF_UPDATES')
                     ]
                 ]
                )
